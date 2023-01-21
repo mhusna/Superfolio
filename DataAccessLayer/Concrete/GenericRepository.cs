841dc41b6785bea408a0d7bfaf437979299f2192
MHusna <mehmethusnak@gmail.com>
2023-01-21 22:43:39 +0300

    Generic repository class'ı oluşturuldu.

---
+++ DataAccessLayer/Concrete/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        Context context = new Context();
+        public void Delete(T entity)
+        {
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById(int id)
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
