8e9df876cd2fda6589594f863cfe54364d468779
MHusna <mehmethusnak@gmail.com>
2023-02-13 14:17:04 +0300

    DefaultController, partial view oluşturuldu.

---
+++ Superfolio/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult NavbarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
