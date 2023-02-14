a1c5b834fa02c968b91c1f51a8c56ef32724d901
MHusna <mehmethusnak@gmail.com>
2023-02-14 11:19:21 +0300

    About bölümü viewcomponent yazıldı.

---
+++ Superfolio/ViewComponents/About/AboutList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.ViewComponents.About
+{
+    public class AboutList:ViewComponent
+    {
+        AboutManager manager = new AboutManager(new EfAboutRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = manager.TGetList();
+            return View(values);
+        }
+    }
+}
