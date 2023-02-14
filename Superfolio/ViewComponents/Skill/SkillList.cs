9ac823cb28563d8d388601954d4d2a8e5db46d65
MHusna <mehmethusnak@gmail.com>
2023-02-14 12:13:56 +0300

    Skill için viewcomponent yazıldı.

---
+++ Superfolio/ViewComponents/Skill/SkillList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.ViewComponents.Skill
+{
+    public class SkillList:ViewComponent
+    {
+        SkillManager manager = new SkillManager(new EfSkillRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var results = manager.TGetList();
+            return View(results);
+        }
+    }
+}
