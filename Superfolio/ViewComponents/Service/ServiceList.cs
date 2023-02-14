46a6af7b5704212ec35bfb02c29cf51efcbf9750
MHusna <mehmethusnak@gmail.com>
2023-02-14 12:07:32 +0300

    Service için viewcomponent yazıldı.

---
+++ Superfolio/ViewComponents/Service/ServiceList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+
+namespace Superfolio.ViewComponents.Service
+{
+    public class ServiceList:ViewComponent
+    {
+        ServiceManager manager = new ServiceManager(new EfServiceRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = manager.TGetList();
+            return View(values);
+        }
+    }
+}
