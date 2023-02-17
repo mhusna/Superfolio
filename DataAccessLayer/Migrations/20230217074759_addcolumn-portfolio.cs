4164376d34e49d488b2d9336f9795794573d00d4
MHusna <mehmethusnak@gmail.com>
2023-02-17 11:05:11 +0300

    Migration dosyaları.

---
+++ DataAccessLayer/Migrations/20230217074759_addcolumn-portfolio.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcolumnportfolio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageSliderUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectUrl",
+                table: "Portfolios",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageSliderUrl",
+                table: "Portfolios");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectUrl",
+                table: "Portfolios");
+        }
+    }
+}
