57d76ff76090771eafc965676f3e8dfa610939a6
MHusna <mehmethusnak@gmail.com>
2023-02-17 12:46:12 +0300

    Testimonial viewcomponent oluşturuldu.

---
+++ Superfolio/ViewComponents/Testimonial/TestimonialList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.ViewComponents.Testimonial
+{
+    public class TestimonialList:ViewComponent
+    {
+        TestimonialManager manager = new TestimonialManager(new EfTestimonialRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var results = manager.TGetList();
+            return View(results);
+        }
+    }
+}
