d57394d8258e962ee658a696ea66cfc40dad6634
MHusna <mehmethusnak@gmail.com>
2023-02-17 11:06:51 +0300

    Portfolio viewcomponent oluşturuldu.

---
+++ Superfolio/ViewComponents/Portfolio/PortfolioList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.ViewComponents.Portfolio
+{
+    public class PortfolioList:ViewComponent
+    {
+        PortfolioManager manager = new PortfolioManager(new EfPortfolioRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var results = manager.TGetList();
+            return View(results);
+        }
+    }
+}
