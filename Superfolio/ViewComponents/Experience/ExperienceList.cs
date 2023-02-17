0dbca2949cf66509ed254854f49465ae5fff0b3e
MHusna <mehmethusnak@gmail.com>
2023-02-17 12:15:22 +0300

    Experience viewcomponent oluşturuldu.

---
+++ Superfolio/ViewComponents/Experience/ExperienceList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.ViewComponents.Experience
+{
+    public class ExperienceList:ViewComponent
+    {
+        ExperienceManager manager = new ExperienceManager(new EfExperienceRepository());
+        
+        public IViewComponentResult Invoke()
+        {
+            var results = manager.TGetList();
+            return View(results);
+        }
+    }
+}
