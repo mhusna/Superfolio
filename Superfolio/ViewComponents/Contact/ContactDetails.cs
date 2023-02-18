cc22f370b668baed23bea76fa0afd0e95fc79e5e
MHusna <mehmethusnak@gmail.com>
2023-02-18 16:06:47 +0300

    ContactDetails viewcomponent oluşturuldu.

---
+++ Superfolio/ViewComponents/Contact/ContactDetails.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Superfolio.ViewComponents.Contact
+{
+    public class ContactDetails:ViewComponent
+    {
+        ContactManager manager = new ContactManager(new EfContactRepository());
+        public IViewComponentResult Invoke()
+        {
+            var results = manager.TGetList();
+            return View(results[0]);
+        }
+    }
+}
